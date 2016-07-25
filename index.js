3c198ca1be34e99037a3a6676211971d26d81adf
i5ting <i5ting@126.com>
2016-07-25 23:22:51 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const load_koa_middlewares = require('load_koa_middlewares')
 
 module.exports = function(key){
   var _key = key || 'config'
+
   const config = require('get_closest_package_json')[_key]
   
   console.log(config)
